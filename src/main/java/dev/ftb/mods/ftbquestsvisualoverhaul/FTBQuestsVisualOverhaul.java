294a346286ac913cfc8e9efaefe7103a9c037999
Ryan Eaton <ryaneaton@spea.cc>
2026-04-15 16:12:07 -0400

    Bump version to 0.1.6

--- src/main/java/dev/ftb/mods/ftbquestsvisualoverhaul/FTBQuestsVisualOverhaul.java
+++ src/main/java/dev/ftb/mods/ftbquestsvisualoverhaul/FTBQuestsVisualOverhaul.java
@@ -1,5 +1,6 @@
 package dev.ftb.mods.ftbquestsvisualoverhaul;
 
+import com.mojang.logging.LogUtils;
 import dev.ftb.mods.ftbquestsvisualoverhaul.client.OverhaulClient;
 import dev.ftb.mods.ftbquestsvisualoverhaul.client.config.ModClientConfig;
 import net.minecraftforge.api.distmarker.Dist;
@@ -8,10 +9,12 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import org.slf4j.Logger;
 
 @Mod(FTBQuestsVisualOverhaul.MOD_ID)
 public class FTBQuestsVisualOverhaul {
     public static final String MOD_ID = "ftbquestsvisualoverhaul";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public FTBQuestsVisualOverhaul() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModClientConfig.SPEC);
